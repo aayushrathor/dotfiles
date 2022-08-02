6628f13f4b3eb82441fa279acf7c66326ecfc29b
aayushrathor <rathoreaayush84@gmail.com>
2022-08-02 18:15:32 +0530

    new: bottom bar

--- configs/polybar/.config/polybar/bluetooth.sh
+++ configs/polybar/.config/polybar/bluetooth.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#bbffffff}"
+  echo "%{F#ABC9FF}"
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
--- configs/polybar/.config/polybar/launch.sh
+++ configs/polybar/.config/polybar/launch.sh
@@ -2,12 +2,24 @@
 
 # Terminate already running bar instances
 killall -q polybar
+
+#wait untill all bars are not closed
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 
-echo "Polybar launched..."
+# Command for top bar
+echo "Top bar launched successfully ...."
+
+polybar top 2>&1 | tee -a /tmp/polybar.log & disown
+
+# Command for bottom bar
+echo "bottom bar launched successfully ...."
+
+polybar -c ~/.config/polybar/config.ini bottom -r &
+
+echo "Both bars launched successfully ...."
 
 
