ef34bc724a604fd5f5b043fffdb794cadbf196b0
aayushrathor <rathoreaayush84@gmail.com>
2023-09-04 13:49:19 +0530

    feat: long time no update

--- configs/hypr/.config/hypr/scripts/screensharing.sh
+++ configs/hypr/.config/hypr/scripts/screensharing.sh
@@ -3,7 +3,8 @@ sleep 1
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/libexec/xdg-desktop-portal-hyprland &
+sleep 1
+/usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
 export SDL_VIDEODRIVER=wayland
